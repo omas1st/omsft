9ebefa53e497b1a62431903084319353809ce920
omas <omas7th@gmail.com>
2026-05-17 08:12:33 +0100

    update

--- src/utils/pairs.js
+++ src/utils/pairs.js
@@ -16,10 +16,35 @@ export const PAIRS = {
     'GBP/NZD', 'AUD/NZD', 'USD/SGD', 'EUR/SGD', 'GBP/SGD'
   ],
   stocks: [
+    // ----- Original large-caps -----
     'AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA',
     'META', 'NFLX', 'NVDA', 'BABA', 'JPM',
     'V', 'WMT', 'JNJ', 'PG', 'MA',
-    'BAC', 'DIS', 'ADBE', 'PYPL', 'INTC'
+    'BAC', 'DIS', 'ADBE', 'PYPL', 'INTC',
+
+    // ----- Broad NASDAQ additions (tech, biotech, consumer, etc.) -----
+    'AUDC', 'PIII',                     // specifically requested
+    'AMD', 'QCOM', 'AVGO', 'TXN', 'LRCX',
+    'KLAC', 'AMAT', 'MU', 'MRVL', 'NXPI',
+    'CRM', 'SNOW', 'DDOG', 'MDB', 'CRWD',
+    'ZS', 'PANW', 'FTNT', 'OKTA', 'NET',
+    'TEAM', 'PLTR', 'U', 'PATH', 'S',
+    'COIN', 'DKNG', 'ABNB', 'LYFT', 'ROKU',
+    'AMGN', 'GILD', 'REGN', 'VRTX', 'ILMN',
+    'MRNA', 'BIIB', 'SGEN',
+    'COST', 'SBUX', 'LULU', 'MAR', 'EXPE',
+    'BKNG', 'CHTR', 'TCOM',
+    'INTU', 'SQ', 'AFRM',
+    'ISRG', 'MELI', 'ASML', 'ADP', 'CSCO',
+    'CTAS', 'WDAY', 'CDNS', 'SNPS', 'ADSK',
+    'ROP', 'FISV', 'KDP', 'PCAR', 'NXST',
+
+    // ----- Ross Cameron small‑cap momentum plays -----
+    'LGVN', 'KAVL', 'VLCN', 'LIXT', 'CNSP',
+    'MEX', 'APHI', 'MRAM', 'ENRA', 'ATRA',
+    'MLGO', 'RGS',
+    'JBDI', 'BSLK', 'VRAX',
+    'SPEC', 'AREB', 'NXL', 'MINM'
   ],
   crypto: [
     'BTC/USD', 'ETH/USD', 'XRP/USD', 'LTC/USD', 'BCH/USD',
